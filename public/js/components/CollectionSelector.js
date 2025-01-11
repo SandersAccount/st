a74f1ab4ef4313a92382c8477929f16631d44dc9
Ariel Bauer <arielbauerariel@gmail.com>
2025-01-11 20:39:28 +0200

    Initial commit with basic project structure

---
+++ public/js/components/CollectionSelector.js
@@ -0,0 +1,12 @@
+// CollectionSelector.js
+// Import necessary dependencies here
+
+class CollectionSelector {
+    constructor() {
+        // Implementation
+    }
+    
+    // Add your component methods here
+}
+
+export default CollectionSelector;
