6d618e212f927271661ad72edd04546eba9af221
Ariel Bauer <34476917+SandersAccount@users.noreply.github.com>
2025-01-27 12:25:47 +0200

    Update ipn.js

--- routes/ipn.js
+++ routes/ipn.js
@@ -82,7 +82,7 @@ router.post('/notification', async (req, res) => {
         // Handle credit assignment
         if (itemNumber === stickerLabProductId) {
             // Logic for handling StickerLab purchase
-            user.credits = (user.credits || 0) + 100;
+            user.credits = (user.credits || 0) + 250;
             console.log('User gained access to StickerLab and received 100 credits:', user.email);
         } else {
             // Handle credit package purchase
